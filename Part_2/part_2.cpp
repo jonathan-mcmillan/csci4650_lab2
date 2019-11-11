658dccd4815fd4076d3f909335d270d920499f29
Oliver Grassmann <olivergrassmann@Olivers-MacBook-Pro.local>
2019-11-11 13:21:16 -0600

    updated defualt files in part_2.cpp, added keys to part2 folder

--- Part_2/part_2.cpp
+++ Part_2/part_2.cpp
@@ -25,12 +25,12 @@ string readFile(string fileName);
 int main(int argc, char *argv[]) {
     string plaintextMessageFN, encryptedSessionKeyFN, thirdPartyPublicKeyFN, yourPrivateKeyFN;
     if(argc != 5) {
-        cout << "Usage: part2 <plaintext_message.txt> <encrypted_session_key.pem> <third-party_public_key.pem> <your_private_key.pem>" << endl << "Incorrect number of parameters entered, using default file name values instead." << endl;
+        cout << "Usage: part2 <plaintext_message.txt> <encrypted_session.key> <third-party_public_key.pem> <your_private_key.pem>" << endl << "Incorrect number of parameters entered, using default file name values instead." << endl;
         
         plaintextMessageFN = "plaintext_message.txt";
-        encryptedSessionKeyFN = "encrypted_session_key.pem";
-        thirdPartyPublicKeyFN = "thrid-party_public_key.pem";
-        yourPrivateKeyFN = "your_private_key.pem";
+        encryptedSessionKeyFN = "encrypted_session.key";
+        thirdPartyPublicKeyFN = "pubkey.pem";
+        yourPrivateKeyFN = "oliver_privkey.pem";
     } else {
         plaintextMessageFN = argv[1];
         encryptedSessionKeyFN = argv[2];
@@ -58,7 +58,6 @@ string readFile(string fileName) {
 	const char *filename = fileName.c_str();
 	FILE *file = fopen(filename, "rb");
 	
-	cout << "hi" << endl;
 	if(file){
 		string contents;
 		fseek(file, 0, SEEK_END);
