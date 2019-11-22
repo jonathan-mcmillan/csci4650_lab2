788b0be1cbb18ce8fb84f3a2971b6dd3e4921097
Jonathan McMillan <jonathan.mcmillan@slu.edu>
2019-11-22 12:49:52 -0600

    removed bloat

--- Part_2/part_2.cpp
+++ Part_2/part_2.cpp
@@ -46,21 +46,21 @@ int main(int argc, char *argv[]) {
         	yourPrivateKeyFN = argv[4];
     	}
 
-    string plaintextMessage = readFile(plaintextMessageFN);
-    cout << "Plaintext Message: " << endl << plaintextMessage << endl;
+  	string plaintextMessage = readFile(plaintextMessageFN);
+    	cout << "Plaintext Message: " << endl << plaintextMessage << endl;
 
-    string encryptedSessionKey = readFile(encryptedSessionKeyFN);
-    cout << "Encrypted Session Key: " << endl << encryptedSessionKey << endl; 
+    	string encryptedSessionKey = readFile(encryptedSessionKeyFN);
+    	cout << "Encrypted Session Key: " << endl << encryptedSessionKey << endl; 
 
-    string thirdPartyPublicKey = readFile(thirdPartyPublicKeyFN);
-    cout << "Third-Party Public Key: " << endl << thirdPartyPublicKey << endl;
+    	string thirdPartyPublicKey = readFile(thirdPartyPublicKeyFN);
+    	cout << "Third-Party Public Key: " << endl << thirdPartyPublicKey << endl;
 
-    string yourPrivateKey = readFile(yourPrivateKeyFN);
-    cout << "Your Private Key: " << endl << yourPrivateKey << endl;
+    	string yourPrivateKey = readFile(yourPrivateKeyFN);
+    	cout << "Your Private Key: " << endl << yourPrivateKey << endl;
 
 	//start of 2 - https://wiki.openssl.org/index.php/EVP_Asymmetric_Encryption_and_Decryption_of_an_Envelope
 	//there is a section called opening and envelope which should help with this
-	//https://www.openssl.org/docs/man1.0.2/man3/EVP_PKEY_decrypt.html
+	//https://www.openssl.org/docs/man1.0.2/man3/EVP_PKEY_encrypt.html
 
 	ENGINE *eng = ENGINE_get_default_RSA();	
 	unsigned char *out, *in;
@@ -79,10 +79,12 @@ int main(int argc, char *argv[]) {
 	if(!ctx){
 		throw(errno);
 	}
+
 	cout << "2" << endl;
 	if(EVP_PKEY_encrypt_init(ctx) <= 0){
 		throw(errno);
 	}
+
 	cout << "3" << endl;
 	if(EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_OAEP_PADDING) <= 0){
 		throw(errno);
@@ -96,6 +98,7 @@ int main(int argc, char *argv[]) {
 	if(EVP_PKEY_encrypt(ctx, NULL, &outlen, in, inlen) <= 0){
 		throw(errno);
 	}
+
 	cout << "5" << endl;
 	out = (unsigned char*) OPENSSL_malloc(outlen);
 
@@ -111,65 +114,14 @@ int main(int argc, char *argv[]) {
 	//end of 2
 	
 	string de_ses((char *) out);
-	cout << de_ses << endl;
-
+	cout << hex << de_ses << endl;
+/*
 	//start of 3
 	//write buffer to a file
 	//http://www.cplusplus.com/reference/cstdio/fwrite/
 	ofstream out_f("decrypted_session.txt");
 	out_f << de_ses;
 	out_f.close();
-
-	//at this point buffer should be written to "decrypted_session.txt"
-	//end of 3
-/*
-	//start of 4
-	ENGINE *eng_en;
-        unsigned char *out_en, *in_en;
-        size_t outlen_en, inlen_en;
-	//FILE *ses = fopen("decrypted_session.txt".c_str(), "rb");
-	EVP_PKEY *ses_key = de_ses;//PEM_read_PUBKEY(ses, NULL, NULL, NULL);
-        if(ses_key == NULL){
-                throw(errno);
-        }
-
-        cout << "1" << endl;
-        EVP_PKEY_CTX *ctx_en = EVP_PKEY_CTX_new(ses_key, eng_en);
-        if(!ctx_en){
-                throw(errno);
-        }
-        cout << "2" << endl;
-        if(EVP_PKEY_encrypt_init(ctx_en) <= 0){
-                throw(errno);
-        }
-        cout << "3" << endl;
-        if(EVP_PKEY_CTX_set_rsa_padding(ctx_en, RSA_PKCS1_OAEP_PADDING) <= 0){
-                throw(errno);
-        }
-        cout << "4" << endl;
-        //find buffer length
-        if(EVP_PKEY_encrypt(ctx_en, NULL, &outlen_en, in_en, inlen_en) <= 0){
-        	throw(errno);
-	}
-        cout << "5" << endl;
-        out_en = (unsigned char*) OPENSSL_malloc(outlen_en);
-        
-	if(!out_en){
-        	throw(errno);
-        }
-
-        if (EVP_PKEY_encrypt(ctx_en, out_en, &outlen_en, in_en, inlen_en) <= 0){
-        	throw(errno);
-		cout << "unable to encrypt plaintext..." << endl << &out_en << endl << out_en << endl;
-        }
-*/
-	//read private key
-/*	cout << "priv key" << endl;
-	FILE *priv = fopen(yourPrivateKeyFN.c_str(), "rb");
-	EVP_PKEY *priv_key = PEM_read_PrivateKey(priv, NULL, NULL, NULL);
-	if(priv_key == NULL){
-		throw(errno);
-	}
 */
 	return 0;
 }
